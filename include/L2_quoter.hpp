cedf5b544240a1b9e31d81265a48a00c5d3fbc23
zhangxuan <295501139@qq.com>
2024-03-28 17:22:06 +0800

    3_28

--- include/L2_quoter.hpp
+++ include/L2_quoter.hpp
@@ -11,6 +11,7 @@
 #include <string.h>
 #include <string>
 #include <iostream>
+#include <unistd.h>
 //#include <map>
 #include "TORATstpLev2MdApi.h"
 //#include "concurrentqueue.h"
@@ -27,67 +28,36 @@
 using namespace TORALEV2API;
 class Lev2MdSpi : public CTORATstpLev2MdSpi{
 public:
+	Lev2MdSpi():m_api(nullptr),m_request_id(0){};
     Lev2MdSpi(CTORATstpLev2MdApi *api):m_api(api),m_request_id(0){};
     ~Lev2MdSpi(){};
 public:
-	//virtual void connect(const char* const userid, const char* const password, const char* const address, const char* const mode);//用户ID、密码、服务器地址和连接模式
-	//request
-	virtual void OnFrontConnected(std::string username,std::string password);
+	virtual void init(char * userid,char * password,char * address);
+	virtual void OnFrontConnected();
 	virtual void OnRspUserLogin(CTORATstpRspUserLoginField *pRspUserLogin, CTORATstpRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
-	//virtual void Stop();
-	//virtual void OnFrontDisconnected(int nReason);
-	/// Error response
-	//virtual void OnRspError(TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-	/// Login request response，登陆响应
-    // Subscribe 
-	/*virtual void Subscribe( char* ppSecurityID[], int nCount, TTORATstpExchangeIDType ExchageID);
-    //UnSubscribe
-	virtual void UnSubscribe(char* ppSecurityID[], int nCount, TTORATstpExchangeIDType ExchageID);
-	********************************** Return Data Callback Function**********************************
-	/// Logout
-	virtual void OnRspUserLogout(TORALEV2API::CTORATstpUserLogoutField* pUserLogout, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-	/// Subscribe NGTSTICK
+
+	//virtual void OnRspUserLogout(TORALEV2API::CTORATstpUserLogoutField* pUserLogout, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
+	//virtual void OnRspSubNGTSTick(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
+	
 	virtual void OnRspSubNGTSTick(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-	*//// OnRspSubMarketData
 	virtual void OnRspSubMarketData(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-	/// OnRspUnSubMarketData
-	/*virtual void OnRspUnSubMarketData(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-	// OnRspSubTransaction
 	virtual void OnRspSubTransaction(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-	/// OnRspSubOrderDetail
 	virtual void OnRspSubOrderDetail(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-	/*
-    ********************************** Data call back **********************************
-	/// OnRtnMarketData
-    */
+
+
+
 	virtual void OnRtnMarketData(CTORATstpLev2MarketDataField* pDepthMarketData, const int FirstLevelBuyNum, const int FirstLevelBuyOrderVolumes[], const int FirstLevelSellNum, const int FirstLevelSellOrderVolumes[]);
-	void add(){
-        char * security_list[]={"510050","600***","688***"};
-        int ret = m_api->SubscribeMarketData (security_list,3, TORA_TSTP_EXD_SSE);
-        if (ret != 0){
-            printf("SubscribeMarketData fail, ret[%d]\n", ret);
-        }
-    }
-    //NGT ֪ͨ
-	/*virtual void OnRtnNGTSTick(CTORATstpLev2NGTSTickField* pTick);
-	/// OnRtnTransaction
+	virtual void OnRtnNGTSTick(CTORATstpLev2NGTSTickField* pTick);
 	virtual void OnRtnTransaction(CTORATstpLev2TransactionField* pTransaction);
-	/// OnRtnOrderDetail
-	virtual void OnRtnOrderDetail(CTORATstpLev2OrderDetailField* pOrderDetail);*/
+	virtual void OnRtnOrderDetail(CTORATstpLev2OrderDetailField* pOrderDetail);
+	void add();
+
 
 private:
 	int m_request_id;
     CTORATstpLev2MdApi *m_api;
-	//CTORATstpLev2MdApi* m_api = NULL;
-	//int m_req_id;
-	//char m_userid[21];
-	//char m_password[41];
-	//char m_address[64];
-	//char m_mode[21];
-	//moodycamel::ConcurrentQueue<SEEvent>* m_Event_Q_ptr = nullptr;
-	//moodycamel::ConcurrentQueue<std::shared_ptr<SEObject>>* m_Event_Q_ptr;
-	//LoggerPtr m_logger=nullptr;
-	//std::map<int,std::tuple<Strategy*,bool>>* m_StrategyMap_Ptr;
+	char  userid[21];
+	char  password[41];
+	char  address[64];
 };
-
 #endif
--- src/L2_quoter.cpp
+++ src/L2_quoter.cpp
@@ -1,84 +1,174 @@
-#include "L2_quoter.hpp"
-    
-	//request
-    //username password Logintype
-    void Lev2MdSpi::OnFrontConnected(std::string username,std::string password){
+#include "/root/SE_Intel/include/L2_quoter.hpp"
+    #include <unistd.h>
+	//初始化，账号，密码，连接地址
+    void Lev2MdSpi::init(char * userid,char * password,char * address){
+        strcpy(this->userid,userid);
+        strcpy(this->password,password);
+        strcpy(this->address,address);
+        //m_api = CTORATstpLev2MdApi::CreateTstpLev2MdApi( TORA_TSTP_MST_MCAST,true);
+        //m_api->RegisterMulticast(address,"172.16.2121.1","");//对于 UDP 连接，调用 RegisterMulticast 方法注册多播地址
+        m_api=CTORATstpLev2MdApi::CreateTstpLev2MdApi(TORA_TSTP_MST_TCP,true);
+        m_api->RegisterFront(address);//调用 RegisterFront 方法注册前置机地址
+        m_api->RegisterSpi(this);//派生自回调接口类的实例
+        
+        m_api->Init();
+        std::cout<<"Init yes"<<std::endl;
+    }
+    //登陆
+    void Lev2MdSpi::OnFrontConnected(){
+        std::cout<<"OnFrontConnected!"<<std::endl;
         CTORATstpReqUserLoginField req_user_login_field;
         memset(&req_user_login_field, 0, sizeof(req_user_login_field));//清空
-        strcpy(req_user_login_field.LogInAccount, username.c_str());//username
-        req_user_login_field.LogInAccountType =  TORA_TSTP_LACT_UserID;//login_type
+        strcpy(req_user_login_field.LogInAccount, userid);//username
+        req_user_login_field.LogInAccountType =  TORALEV2API::TORA_TSTP_LACT_UserID;//login_type
         //req_user_login_field.AuthMode = TORA_TSTP_SP_AM_Password;
-        strcpy(req_user_login_field.Password, password.c_str());//password
-        strcpy(req_user_login_field.UserProductInfo, "lev2apidemo");//用户端产品信息
+        strcpy(req_user_login_field.Password, password);//password
+        strcpy(req_user_login_field.UserProductInfo, "HX5ZJ0C1PV");//用户端产品信息
+        std::cout<<req_user_login_field.Password<<std::endl;
+        std::cout<<req_user_login_field.LogInAccount<<std::endl;
         int ret = m_api->ReqUserLogin(&req_user_login_field, ++m_request_id);//调用登陆函数，参数为登陆请求信息
         if (ret != 0)//登陆失败返回0
         {
             printf("ReqUserLogin fail, ret[%d]\n", ret);
         }
     }
-     void Lev2MdSpi:: OnRspUserLogin(CTORATstpRspUserLoginField *pRspUserLogin, CTORATstpRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
-        {
-        if (pRspInfo->ErrorID == 0)
-        {
+    //登陆响应
+    void Lev2MdSpi:: OnRspUserLogin(CTORATstpRspUserLoginField *pRspUserLogin, CTORATstpRspInfoField *pRspInfo, int nRequestID, bool bIsLast){
+        if (pRspInfo->ErrorID == 0){
             printf("login success\n");
         }
-        else
-        {
+        else{
             printf("login fail, error_id[%d] error_msg[%s]\n",pRspInfo ->ErrorID, pRspInfo ->ErrorMsg);
         }
     }
-	/// Error response
-    /*
-	void Lev2MdSpi::OnRspError(TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-	/// Login request response，登陆响应
-	void Lev2MdSpi::OnRspUserLogin(TORALEV2API::CTORATstpRspUserLoginField* pRspUserLogin, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-    // Subscribe 
-	void Lev2MdSpi::Subscribe( char* ppSecurityID[], int nCount, TTORATstpExchangeIDType ExchageID);
-    //UnSubscribe
-	void Lev2MdSpi::UnSubscribe(char* ppSecurityID[], int nCount, TTORATstpExchangeIDType ExchageID);
-	/*********************************** Return Data Callback Function**********************************
-	/// Logout
-    void Lev2MdSpi::OnRspUserLogout(TORALEV2API::CTORATstpUserLogoutField* pUserLogout, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-	/// Subscribe NGTSTICK
-	void Lev2MdSpi::OnRspSubNGTSTick(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-    */
-	/// OnRspSubMarketData
-    void Lev2MdSpi::OnRspSubMarketData(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast){
-        if (pRspInfo->ErrorID == 0)
+    void Lev2MdSpi::add(){
+        char* security_list[1];
+		//for(int i=0;i<3;i++){
+		//	security_list[i]=(char *)malloc(sizeof(char)*16);
+		//}
+        char nonconst_id[31];
+        strcpy(nonconst_id ,"002387");
+        security_list[0] = nonconst_id;
+		//strcpy(security_list[0],"002387");
+		//strcpy(security_list[1],"600124");
+		//strcpy(security_list[2],"688345");
+        //Subscribe NGTS (orderdetial and trasaction together)
+        int ret_nt = m_api->SubscribeNGTSTick(security_list, sizeof(security_list) / sizeof(char*), TORA_TSTP_EXD_SSE);
+        if (ret_nt == 0)
         {
-        printf("sub market data success! \n");
+            std::cout<<"yes"<<std::endl;
         }
         else
         {
-        printf("sub market data fail, error_id[%d] error_msg[%s] \n", 
+            std::cout<<"no"<<std::endl;
+        }
+		
+		
+		//Subscribe old orderdetial 
+		int ret_od = m_api->SubscribeOrderDetail(security_list, sizeof(security_list) / sizeof(char*),TORA_TSTP_EXD_SZSE);
+		if (ret_od == 0)
+		{
+			std::cout<<"yes"<<std::endl;
+		}
+		else
+		{
+			std::cout<<"no"<<std::endl;
+		}
+
+		//Subscribe old trasaction 
+		int ret_t = m_api->SubscribeTransaction(security_list, sizeof(security_list) / sizeof(char*), TORA_TSTP_EXD_SZSE);
+        if (ret_t == 0)
+		{
+			std::cout<<"yes"<<std::endl;
+		}
+		else
+		{
+			std::cout<<"no"<<std::endl;
+		}
+
+		//Subscribe to market data
+		int ret_md = m_api->SubscribeMarketData(security_list, sizeof(security_list) / sizeof(char*), TORA_TSTP_EXD_SZSE);
+		if (ret_md == 0)
+		{
+			std::cout<<"yes"<<std::endl;
+		}
+		else
+		{
+			std::cout<<"no"<<std::endl;
+		}
+    }
+    
+    //------------------------------------------------------------------------响应报文----------------------------------------------------------------------------------//
+    void Lev2MdSpi::OnRspSubNGTSTick(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast){
+        if (pRspInfo->ErrorID == 0)
+        {
+            printf("OnRspsubRtnNGTSTick sub market data success! \n");
+        }
+        else
+        {
+         printf("sub market data fail, error_id[%d] error_msg[%s] \n", 
         pRspInfo->ErrorID, pRspInfo->ErrorMsg); }
-}
-	/// OnRspUnSubMarketData
-	/*void Lev2MdSpi::OnRspUnSubMarketData(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-	// OnRspSubTransaction
-	void Lev2MdSpi::OnRspSubTransaction(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
-	/// OnRspSubOrderDetail
-	void Lev2MdSpi::OnRspSubOrderDetail(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast);*/
-	/*********************************** Data call back ***********************************/
-	/// OnRtnMarketData
-    //
+    }
+	void Lev2MdSpi::OnRspSubMarketData(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast){
+        if (pRspInfo->ErrorID == 0)
+        {
+            printf("OnRspSubMarketData sub market data success! \n");
+        }
+        else
+        {
+         printf("sub market data fail, error_id[%d] error_msg[%s] \n", 
+        pRspInfo->ErrorID, pRspInfo->ErrorMsg); }
+    }
+	void Lev2MdSpi::OnRspSubTransaction(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast){
+        if (pRspInfo->ErrorID == 0)
+        {
+            printf("OnRspSubTransaction sub market data success! \n");
+        }
+        else
+        {
+         printf("sub market data fail, error_id[%d] error_msg[%s] \n", 
+        pRspInfo->ErrorID, pRspInfo->ErrorMsg); }
+    }
+	void Lev2MdSpi::OnRspSubOrderDetail(CTORATstpSpecificSecurityField* pSpecificSecurity, TORALEV2API::CTORATstpRspInfoField* pRspInfo, int nRequestID, bool bIsLast){
+        if (pRspInfo->ErrorID == 0)
+        {
+            printf("OnRspSubOrderDetail sub market data success! \n");
+        }
+        else
+        {
+         printf("sub market data fail, error_id[%d] error_msg[%s] \n", 
+        pRspInfo->ErrorID, pRspInfo->ErrorMsg); }
+    }
+    //------------------------------------------------------------------------应答报文----------------------------------------------------------------------------------//
     void Lev2MdSpi::OnRtnMarketData(CTORATstpLev2MarketDataField* pDepthMarketData, const int FirstLevelBuyNum, const int FirstLevelBuyOrderVolumes[], const int FirstLevelSellNum, const int FirstLevelSellOrderVolumes[]){
+        std::cout<<"yes"<<std::endl;
         printf("OnRtnMarketData:security_id[%s] last_price[%f] time_stamp[%d] total_value_trade[%lld]\n",
             pDepthMarketData ->SecurityID,
             pDepthMarketData ->LastPrice,
             pDepthMarketData ->DataTimeStamp,
             pDepthMarketData ->TotalValueTrade);
-	}//NGT ֪ͨ
-	/*void Lev2MdSpi::OnRtnNGTSTick(CTORATstpLev2NGTSTickField* pTick);
-	/// OnRtnTransaction
-	void Lev2MdSpi::OnRtnTransaction(CTORATstpLev2TransactionField* pTransaction);
-	/// OnRtnOrderDetail
-	void Lev2MdSpi::OnRtnOrderDetail(CTORATstpLev2OrderDetailField* pOrderDetail);*/
+	}
+    void Lev2MdSpi::OnRtnNGTSTick(CTORATstpLev2NGTSTickField* pTick){
+        std::cout<<1<<std::endl;
+    }
+	void Lev2MdSpi::OnRtnTransaction(CTORATstpLev2TransactionField* pTransaction){
+        std::cout<<2<<std::endl;
+    }
+	void Lev2MdSpi::OnRtnOrderDetail(CTORATstpLev2OrderDetailField* pOrderDetail){
+        std::cout<<3<<std::endl;
+    }
     int main(){
-        CTORATstpLev2MdApi *api = CTORATstpLev2MdApi::CreateTstpLev2MdApi();
-        Lev2MdSpi spi(api);
-        api->RegisterSpi(&spi);
-        api->RegisterFront("tcp://127.0.0.1:8500");
-        api->Init();
-        std::cout<<"???"<<std::endl;
+        Lev2MdSpi spi;
+        char userid[21],passwd[32],addrs[64];
+        strcpy(userid,"00032129");
+        strcpy(passwd,"19359120");
+        strcpy(addrs,"tcp://210.14.72.17:6900");
+       //strcpy(addrs, "tcp://127.0.0.1:8500");
+        spi.init(userid,passwd,addrs);
+        sleep(3);
+        spi.add();
+        while(1){
+
+        }
+        std::cout<<"this ok"<<std::endl;
     }
